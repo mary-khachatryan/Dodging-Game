89e8fd61ea58fbc79fc1d9ca9a35e071ec187a3f
mary-khachatryan <mary.khachatryan2@gmail.com>
2023-05-06 21:47:15 +0400

    Spawning Obstacles Repeatedly

---
+++ Assets/scripts/ObstacleSpawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    public float spawnRate;
+    public float maxXpos;
+
+	private void Start()
+	{
+        StartSpawning();
+	}
+
+	void Spawn()
+    {
+
+		float randomX = Random.Range(-maxXpos, maxXpos);
+		Vector2 spawnPos = new Vector2(randomX, transform.position.y);
+
+		Instantiate(obstacle, spawnPos, Quaternion.identity);
+
+
+	}
+
+    void StartSpawning() 
+    {
+        InvokeRepeating("Spawn", 1f, spawnRate);
+
+    }
+    public void StopSpawning()
+    {
+
+        CancelInvoke("Spawn");
+    }
+}
